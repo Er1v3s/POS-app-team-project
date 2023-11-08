97588d6b291e3fd0b216ea612ee6b78141952ac1
Filip Statkiewicz <stf6.dev@gmail.com>
2023-11-08 21:27:14 +0100

    Create example tasks for ToDo List

--- POS/Views/MainWindow.xaml.cs
+++ POS/Views/MainWindow.xaml.cs
@@ -25,18 +25,28 @@ namespace POS
         {
             InitializeComponent();
 
-            var converter = new BrushConverter();
             ObservableCollection<Employee> employee = new ObservableCollection<Employee>();
+            ObservableCollection<ToDoListTask> toDoListTasks = new ObservableCollection<ToDoListTask>();
 
-            // Create DataGrid Item Info
+            // Create working time summary DataGrid Item Info
 
             employee.Add(new Employee { firstName = "Andrzej",  workingTimeFrom = "18:00", workingTimeTo = "24:00", workingTimeSummary = "6:00"});           
             employee.Add(new Employee { firstName = "Łukasz", workingTimeFrom = "19:00", workingTimeTo = "23:00", workingTimeSummary = "4:00"});           
             employee.Add(new Employee { firstName = "Klara", workingTimeFrom = "20:00", workingTimeTo = "22:00", workingTimeSummary = "2:00"});           
-            employee.Add(new Employee { firstName = "Mateusz", workingTimeFrom = "17:30", workingTimeTo = "22:30", workingTimeSummary = "5:00"});           
-            employee.Add(new Employee { firstName = "Robert", workingTimeFrom = "15:00", workingTimeTo = "20:00", workingTimeSummary = "5:00"})
+            employee.Add(new Employee { firstName = "Mateusz", workingTimeFrom = "17:30", workingTimeTo = "22:30", workingTimeSummary = "5:00"});
+            employee.Add(new Employee { firstName = "Robert", workingTimeFrom = "15:00", workingTimeTo = "20:00", workingTimeSummary = "5:00" });
 
             workingTimeSummaryDataGrid.ItemsSource = employee;
+
+            // Create todo list DataGrid Item Info
+
+            toDoListTasks.Add(new ToDoListTask { content = "Zrobić zamówienie" });
+            toDoListTasks.Add(new ToDoListTask { content = "Wezwać serwis do nalewaka na piwo" });
+            toDoListTasks.Add(new ToDoListTask { content = "Wysłać faktury do księgowej" });
+            toDoListTasks.Add(new ToDoListTask { content = "Sprawdzić terminy ważności w lodówce" });
+            toDoListTasks.Add(new ToDoListTask { content = "Wynieść śmieci" });
+
+            todoListDataGrid.ItemsSource = toDoListTasks;
         }
     }
 
@@ -46,5 +56,10 @@ namespace POS
         public string workingTimeFrom { get; set; }
         public string workingTimeTo { get; set; }
         public string workingTimeSummary { get; set; }
+    }
+
+    public class ToDoListTask 
+    {
+        public string content { get; set; }
     }
 }
