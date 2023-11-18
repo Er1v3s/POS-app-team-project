b26d742220ee56ba3dfc4480130a1a00a61298fa
Dawid Romanów <qas.v.pl@gmail.com>
2023-11-18 21:38:27 +0100

    Search products

--- POS/Views/SalesPanel.xaml.cs
+++ POS/Views/SalesPanel.xaml.cs
@@ -27,13 +27,17 @@ namespace POS.Views
         public SalesPanel()
         {
             InitializeComponent();
+            LoadProducts();
+
+            searchTextBox.GotFocus += SearchTextBox_GotFocus;
+            searchTextBox.LostFocus += SearchTextBox_LostFocus;
 
             orderList.Add(new OrderItem { Id = 1, Name = "Wodka & Cola", Amount = 1, Price = 12.99 });
             orderList.Add(new OrderItem { Id = 1, Name = "Margharita", Amount = 1, Price = 31.99 });
             orderList.Add(new OrderItem { Id = 1, Name = "Wodka Stock 0,7", Amount = 1, Price = 69.99 });
-            orderList.Add(new OrderItem { Id = 1, Name = "Cuba Libre", Amount = 2, Price = 22.99 }); 
-            orderList.Add(new OrderItem { Id = 1, Name = "Wodka & Cola", Amount = 1, Price = 12.99 }); 
-            orderList.Add(new OrderItem { Id = 1, Name = "Margharita", Amount = 5, Price = 31.99 }); 
+            orderList.Add(new OrderItem { Id = 1, Name = "Cuba Libre", Amount = 2, Price = 22.99 });
+            orderList.Add(new OrderItem { Id = 1, Name = "Wodka & Cola", Amount = 1, Price = 12.99 });
+            orderList.Add(new OrderItem { Id = 1, Name = "Margharita", Amount = 5, Price = 31.99 });
             orderList.Add(new OrderItem { Id = 1, Name = "Wodka Stock 0,7", Amount = 1, Price = 69.99 });
 
             orderListDataGrid.ItemsSource = orderList;
@@ -52,18 +56,145 @@ namespace POS.Views
             totalAmountLabel.Content = $"{totalAmount:C2}";
         }
 
+        // onClick menuitem button method
+       /* private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            orderList.Add(new OrderItem { Id = 1, Name = "Cuba Libre", Amount = 1, Price = 18.99 });
+        } */
         private void MoveToMainWindow(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
-            
+
             Window.GetWindow(this).Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SearchTextBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            orderList.Add(new OrderItem { Id = 1, Name = "Cuba Libre", Amount = 1, Price = 18.99 });
+            if (searchTextBox.Text == "Szukaj")
+            {
+                searchTextBox.Text = "";
+            }
         }
+
+        private void SearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(searchTextBox.Text))
+            {
+                searchTextBox.Text = "Szukaj";
+            }
+        }
+
+        private void LoadProducts()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var products = dbContext.Products.ToList();
+
+                foreach (var product in products)
+                {
+                    Button button = new Button
+                    {
+                        Style = (Style)FindResource("chooseProductButton"), // Styl przycisku zasobu z XAML
+                        Content = new StackPanel
+                        {
+                            Children =
+                            {
+                                new TextBlock { TextAlignment = TextAlignment.Center, Margin = new Thickness(10), Text = product.Product_name },
+                                new TextBlock { TextAlignment = TextAlignment.Center, Margin = new Thickness(10), Text = $"{product.Price} zł" }
+                            }
+                        }
+                    };
+
+                    // button onClick:
+                    button.Click += (object sender, RoutedEventArgs e) =>
+                    {
+                        orderList.Add(new OrderItem { Id = 1, Name = product.Product_name, Amount = 1, Price = Convert.ToDouble(product.Price) });
+                    };
+
+                    ProductsWrapPanel.Children.Add(button);
+                }
+            }
+        }
+
+        private void SearchTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var searchText = searchTextBox.Text.ToLower();
+
+                using (var dbContext = new AppDbContext())
+                {
+                    var products = dbContext.Products.Where(p => p.Product_name.ToLower().Contains(searchText)).ToList();
+
+                    ProductsWrapPanel.Children.Clear();
+
+                    foreach (var product in products)
+                    {
+                        Button button = new Button
+                        {
+                            Style = (Style)FindResource("chooseProductButton"),
+                            Content = new StackPanel
+                            {
+                                Children =
+                        {
+                            new TextBlock { TextAlignment = TextAlignment.Center, Margin = new Thickness(10), Text = product.Product_name },
+                            new TextBlock { TextAlignment = TextAlignment.Center, Margin = new Thickness(10), Text = $"{product.Price} zł" }
+                        }
+                            }
+                        };
+
+                        button.Click += (object buttonSender, RoutedEventArgs buttonE) =>
+                        {
+                            orderList.Add(new OrderItem { Id = 1, Name = product.Product_name, Amount = 1, Price = Convert.ToDouble(product.Price) });
+                        };
+
+                        ProductsWrapPanel.Children.Add(button);
+                    }
+                }
+
+                e.Handled = true;
+                searchTextBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+            }
+        }
+        private void CategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button categoryButton = sender as Button;
+            string category = categoryButton.Content.ToString();
+
+            using (var dbContext = new AppDbContext())
+            {
+                var products = dbContext.Products.Where(p => p.Category == category).ToList();
+
+                // Wyczyść aktualnie wyświetlone produkty
+                ProductsWrapPanel.Children.Clear();
+
+                foreach (var product in products)
+                {
+                    Button button = new Button
+                    {
+                        Style = (Style)FindResource("chooseProductButton"),
+                        Content = new StackPanel
+                        {
+                            Children =
+                    {
+                        new TextBlock { TextAlignment = TextAlignment.Center, Margin = new Thickness(10), Text = product.Product_name },
+                        new TextBlock { TextAlignment = TextAlignment.Center, Margin = new Thickness(10), Text = $"{product.Price} zł" }
+                    }
+                        }
+                    };
+
+                    // button onClick:
+                    button.Click += (object buttonSender, RoutedEventArgs buttonE) =>
+                    {
+                        orderList.Add(new OrderItem { Id = 1, Name = product.Product_name, Amount = 1, Price = Convert.ToDouble(product.Price) });
+                    };
+
+                    ProductsWrapPanel.Children.Add(button);
+                }
+            }
+        }
+
     }
 
     public class OrderItem
