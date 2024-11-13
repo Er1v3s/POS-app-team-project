ab915d5fbded90aa2e8924bc1c091701c3f55e32
Filip Statkiewicz <stf6.dev@gmail.com>
2024-11-13 22:56:48 +0100

    Implement factory pattern for report and analysis app module

--- POS/ViewModels/ReportsAndAnalysis/ReportsAndAnalysisViewModel.cs
+++ POS/ViewModels/ReportsAndAnalysis/ReportsAndAnalysisViewModel.cs
@@ -4,8 +4,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using LiveCharts;
-using POS.Models.Reports;
 using POS.ViewModels.ReportsAndAnalysis.Interfaces;
+using POS.ViewModels.ReportsAndAnalysis.Validators;
 
 namespace POS.ViewModels.ReportsAndAnalysis
 {
@@ -16,11 +16,9 @@ namespace POS.ViewModels.ReportsAndAnalysis
         private DateTime endDate = DateTime.Now;
 
         private SeriesCollection seriesCollection;
-        private List<string> labels;
+        private List<string> labels = [];
 
-        public Func<double, string>? Values { get; set; }
-
-        public List<string>? Labels
+        public List<string> Labels
         {
             get => labels;
             set
@@ -55,50 +53,15 @@ namespace POS.ViewModels.ReportsAndAnalysis
         }
 
         public ICommand GenerateReportCommand { get; }
+        private readonly IReportFactory reportFactory;
 
-        private readonly IReportGenerator<ProductSalesDto> _saleReportGenerator;
-        private readonly IReportGenerator<RevenueReportDto> _revenueReportGenerator;
-        private readonly IReportGenerator<OrderReportDto> _numberOfOrdersReportGenerator;
-        private readonly IReportGenerator<EmployeeProductivityDto> _employeeProductivityReportGenerator;
-        private readonly IReportGenerator<PaymentRatioDto> _paymentRatioReportGenerator;
-
-        private readonly IChartGenerator<ProductSalesDto> _salesReportChartGenerator;
-        private readonly IChartGenerator<RevenueReportDto> _revenueReportChartGenerator;
-        private readonly IChartGenerator<OrderReportDto> _numberOfOrdersReportChartGenerator;
-        private readonly IChartGenerator<EmployeeProductivityDto> _employeeProductivityReportChartGenerator;
-        private readonly IChartGenerator<PaymentRatioDto> _paymentRatioReportChartGenerator;
-
-        public ReportsAndAnalysisViewModel(
-            IReportGenerator<ProductSalesDto> saleReportGenerator, 
-            IReportGenerator<RevenueReportDto> revenueReportGenerator, 
-            IReportGenerator<OrderReportDto> numberOfOrdersReportGenerator,
-            IReportGenerator<EmployeeProductivityDto> employeeProductivityReportGenerator,
-            IReportGenerator<PaymentRatioDto> paymentRatioReportGenerator,
-
-            IChartGenerator<ProductSalesDto> salesReportChartGenerator,
-            IChartGenerator<RevenueReportDto> revenueReportChartGenerator,
-            IChartGenerator<OrderReportDto> numberOfOrdersReportChartGenerator,
-            IChartGenerator<EmployeeProductivityDto> employeeProductivityReportChartGenerator,
-            IChartGenerator<PaymentRatioDto> paymentRatioReportChartGenerator
-            )
+        public ReportsAndAnalysisViewModel(IReportFactory reportFactory)
         {
             GenerateReportCommand = new RelayCommand(async _ => await GenerateReport());
-
-            _saleReportGenerator = saleReportGenerator;
-            _revenueReportGenerator = revenueReportGenerator;
-            _numberOfOrdersReportGenerator = numberOfOrdersReportGenerator;
-            _employeeProductivityReportGenerator = employeeProductivityReportGenerator;
-            _paymentRatioReportGenerator = paymentRatioReportGenerator;
-
-            _salesReportChartGenerator = salesReportChartGenerator;
-            _revenueReportChartGenerator = revenueReportChartGenerator;
-            _numberOfOrdersReportChartGenerator = numberOfOrdersReportChartGenerator;
-            _employeeProductivityReportChartGenerator = employeeProductivityReportChartGenerator;
-            _paymentRatioReportChartGenerator = paymentRatioReportChartGenerator;
-
+            this.reportFactory = reportFactory;
             seriesCollection = new SeriesCollection();
 
-            _ = GenerateDefaultChart();
+            _ = GenerateReport(); // default report
         }
 
         private async Task GenerateReport()
@@ -114,61 +77,11 @@ namespace POS.ViewModels.ReportsAndAnalysis
 
             seriesCollection.Clear();
 
-            switch (selectedReportIndex)
-            {
-                case 0:
-                    var productSalesData = await _saleReportGenerator.GenerateData(startDate, endDate);
-                    _salesReportChartGenerator.GenerateChart(productSalesData, seriesCollection, out labels);
-                    break;
-                case 1:
-                    var revenueDailyData = await _revenueReportGenerator.GenerateData(startDate, endDate, "day");
-                    _revenueReportChartGenerator.GenerateChart(revenueDailyData, seriesCollection, out labels, r => r.Date.ToString("yyyy-MM-dd"));
-                    break;
-                case 2:
-                    var revenueWeeklyData = await _revenueReportGenerator.GenerateData(startDate, endDate, "week");
-                    _revenueReportChartGenerator.GenerateChart(revenueWeeklyData, seriesCollection, out labels, r => r.DayOfWeek.ToString());
-                    break;
-                case 3:
-                    var revenueMonthlyData = await _revenueReportGenerator.GenerateData(startDate, endDate, "month");
-                    _revenueReportChartGenerator.GenerateChart(revenueMonthlyData, seriesCollection, out labels, r => r.Date.ToString("yyyy-MM"));
-                    break;
-                case 4:
-                    var revenueYearlyData = await _revenueReportGenerator.GenerateData(startDate, endDate, "year");
-                    _revenueReportChartGenerator.GenerateChart(revenueYearlyData, seriesCollection, out labels, r => r.Date.ToString("yyyy"));
-                    break;
-                case 5:
-                    var orderReportsByDays = await _numberOfOrdersReportGenerator.GenerateData(startDate, endDate, "day");
-                    _numberOfOrdersReportChartGenerator.GenerateChart(orderReportsByDays, seriesCollection, out labels, o => o.Date.ToString("yyyy-MM-dd"));
-                    break;
-                case 6:
-                    var orderReportsByDayOfWeeks = await _numberOfOrdersReportGenerator.GenerateData(startDate, endDate, "week");
-                    _numberOfOrdersReportChartGenerator.GenerateChart(orderReportsByDayOfWeeks, seriesCollection, out labels, o => o.DayOfWeek.ToString());
-                    break;
-                case 7:
-                    var orderReportsByMonths = await _numberOfOrdersReportGenerator.GenerateData(startDate, endDate, "month");
-                    _numberOfOrdersReportChartGenerator.GenerateChart(orderReportsByMonths, seriesCollection, out labels, o => o.Date.ToString("yyyy-MM"));
-                    break;
-                case 8:
-                    var orderReportsByYears = await _numberOfOrdersReportGenerator.GenerateData(startDate, endDate, "year");
-                    _numberOfOrdersReportChartGenerator.GenerateChart(orderReportsByYears, seriesCollection, out labels, o => o.Date.ToString("yyyy"));
-                    break;
-                case 9:
-                    var employeeProductivityData = await _employeeProductivityReportGenerator.GenerateData(startDate, endDate);
-                    _employeeProductivityReportChartGenerator.GenerateChart(employeeProductivityData, seriesCollection, out labels);
-                    break;
-                case 10:
-                    var paymentMethodData = await _paymentRatioReportGenerator.GenerateData(startDate, endDate);
-                    _paymentRatioReportChartGenerator.GenerateChart(paymentMethodData, seriesCollection, out labels);
-                    break;
-            }
+            reportFactory.SetParameters(seriesCollection, startDate, endDate);
+            await reportFactory.GenerateReport(selectedReportIndex);
+            labels = reportFactory.GetUpdatedLabelsValues();
 
             OnPropertyChanged(nameof(labels));
-        }
-
-        private async Task GenerateDefaultChart()
-        {
-            var productSalesData = await _saleReportGenerator.GenerateData(DateTime.Now.AddMonths(-1), DateTime.Now);
-            _salesReportChartGenerator.GenerateChart(productSalesData, seriesCollection, out labels);
         }
     }
 }
