cdb1f205444b0b991ad36b4406215ba78ed5a3a2
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-01 22:37:30 +0100

    fix: Set ingredient properties as 'required'. Add the required properties when creating the model.

--- DataAccess/Models/Ingredient.cs
+++ DataAccess/Models/Ingredient.cs
@@ -3,10 +3,10 @@
     public class Ingredient
     {
         public int IngredientId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Unit { get; set; } = "";
-        public string Package { get; set; } = "";
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Unit { get; set; }
+        public required string Package { get; set; }
         public int Stock { get; set; } = 0;
         public int SafetyStock { get; set; } = 0;
         public string? ExpirationDate {  get; set; }
