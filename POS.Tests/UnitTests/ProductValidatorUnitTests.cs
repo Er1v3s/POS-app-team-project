4b76d766e504cad774dd9610549b151293bf173c
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-01 16:01:46 +0100

    test: Create a unit test of ProductName validation

--- POS.Tests/UnitTests/ProductValidatorUnitTests.cs
+++ POS.Tests/UnitTests/ProductValidatorUnitTests.cs
@@ -23,6 +23,51 @@ namespace POS.Tests.UnitTests
             Recipe = new Recipe() { RecipeName = "test name", RecipeContent = "test content" },
         };
 
-        
+        public static IEnumerable<object[]> InvalidNames()
+        {
+            yield return new object[] { "" };
+            yield return new object[] { "abcABC123!@#$%^&*()_-+={}|[];:'.<>/`~" };
+            yield return new object[] { new string('A', 101) };
+        }
+
+        public static IEnumerable<object[]> ValidNames()
+        {
+            yield return new object[] { "ValidName" };
+            yield return new object[] { "Valid Name 123" };
+            yield return new object[] { "ValidName123" };
+            yield return new object[] { new string('A', 100) };
+        }
+
+        #region Validate Product Name
+
+        [Theory]
+        [MemberData(nameof(InvalidNames))]
+        public void ProductName_ForInvalidValues_ShouldHaveValidationError(string invalidName)
+        {
+            // Arrange
+            product.ProductName = invalidName;
+
+            // Act
+            var result = _productValidator.TestValidate(product);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(p => p.ProductName);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidNames))]
+        public void ProductName_ForValidValues_ShouldNotHaveValidationError(string validName)
+        {
+            // Arrange
+            product.ProductName = validName;
+
+            // Act
+            var result = _productValidator.TestValidate(product);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(p => p.ProductName);
+        }
+
+        #endregion
     }
 }
