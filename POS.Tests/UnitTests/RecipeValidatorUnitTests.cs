6289ca6da91e0ee3c38e311ca36a2c78e8c7c7a0
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-01 19:06:47 +0100

    test: Prepare RecipeValidatorUnitTests class

---
+++ POS.Tests/UnitTests/RecipeValidatorUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using POS.Validators.Models;
+
+namespace POS.Tests.UnitTests
+{
+    public class RecipeValidatorUnitTests
+    {
+        private readonly RecipeValidator _recipeValidator;
+
+        public RecipeValidatorUnitTests()
+        {
+            _recipeValidator = new RecipeValidator();
+        }
+
+        #region Validate Recipe Name
+
+        public static IEnumerable<object[]> InvalidNames()
+        {
+            yield return new object[] { "" };
+            yield return new object[] { "abcABC123!@#$%^&*()_-+={}|[];:'.<>/`~" };
+            yield return new object[] { new string('A', 201) };
+        }
+
+        public static IEnumerable<object[]> ValidNames()
+        {
+            yield return new object[] { "ValidName" };
+            yield return new object[] { "Valid Name 123" };
+            yield return new object[] { "ValidName123" };
+            yield return new object[] { new string('A', 200) };
+        }
+
+        #endregion
+    }
+}
