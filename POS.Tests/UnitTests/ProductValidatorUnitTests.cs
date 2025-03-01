f575ae9259372cb8a2ef5c4e7826a0b8983b2f41
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-01 15:49:28 +0100

    test: Prepare ProductValidatorUnitTest class

---
+++ POS.Tests/UnitTests/ProductValidatorUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Models;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using POS.Validators.Models;
+
+namespace POS.Tests.UnitTests
+{
+    public class ProductValidatorUnitTests
+    {
+        private readonly ProductValidator _productValidator;
+
+        public ProductValidatorUnitTests()
+        {
+            _productValidator = new ProductValidator();
+        }
+
+        private readonly Product product = new()
+        {
+            ProductName = "test name",
+            Category = "test category",
+            Description = "test description",
+            Price = 25.99,
+            Recipe = new Recipe() { RecipeName = "test name", RecipeContent = "test content" },
+        };
+
+        
+    }
+}
