1d766eae60f03c3f7c3327ed003780f2ba1ea48b
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-01 20:55:38 +0100

    test: Create unit tests of the recipe name validation

--- POS.Tests/UnitTests/RecipeValidatorUnitTests.cs
+++ POS.Tests/UnitTests/RecipeValidatorUnitTests.cs
@@ -1,4 +1,6 @@
-﻿using POS.Validators.Models;
+﻿using DataAccess.Models;
+using FluentValidation.TestHelper;
+using POS.Validators.Models;
 
 namespace POS.Tests.UnitTests
 {
@@ -10,6 +12,12 @@ namespace POS.Tests.UnitTests
         {
             _recipeValidator = new RecipeValidator();
         }
+
+        private readonly Recipe recipe = new()
+        {
+            RecipeName = "test name",
+            RecipeContent = "test content"
+        };
 
         #region Validate Recipe Name
 
@@ -27,6 +35,35 @@ namespace POS.Tests.UnitTests
             yield return new object[] { "ValidName123" };
             yield return new object[] { new string('A', 200) };
         }
+
+        [Theory]
+        [MemberData(nameof(InvalidNames))]
+        public void RecipeName_ForInvalidValues_ShouldHaveValidationError(string invalidName)
+        {
+            // Arrange
+            recipe.RecipeName = invalidName;
+
+            // Act
+            var result = _recipeValidator.TestValidate(recipe);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.RecipeName);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidNames))]
+        public void RecipeName_ForValidValues_ShouldNotHaveValidationError(string validName)
+        {
+            // Arrange
+            recipe.RecipeName = validName;
+
+            // Act
+            var result = _recipeValidator.TestValidate(recipe);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(r => r.RecipeName);
+        }
+
 
         #endregion
     }
