7fff4a19f7c8c2e674faecba50472e4da04dac9e
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-02 00:47:55 +0100

    test: Create unit tests of RecipeIngredient data model validation

--- POS.Tests/UnitTests/RecipeIngredientValidatorUnitTests.cs
+++ POS.Tests/UnitTests/RecipeIngredientValidatorUnitTests.cs
@@ -1,4 +1,7 @@
-﻿using DataAccess.Models;
+﻿using System.Globalization;
+using DataAccess.Models;
+using FluentAssertions;
+using FluentValidation.TestHelper;
 using POS.Validators.Models;
 
 namespace POS.Tests.UnitTests
@@ -19,6 +22,54 @@ namespace POS.Tests.UnitTests
             Quantity = 0
         };
 
+        #region Validate Quantity
 
+        public static IEnumerable<object[]> InvalidQuantity()
+        {
+            yield return ["-1"];
+            yield return ["50.501"];
+            yield return ["250,501"];
+            yield return ["1001"];
+        }
+
+        public static IEnumerable<object[]> ValidQuantity()
+        {
+            yield return ["0"];
+            yield return ["50.50"];
+            yield return ["300,50"];
+            yield return ["1000"];
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidQuantity))]
+        public void RecipeIngredientQuantity_ForInvalidValues_ShouldHaveValidationError(string quantity)
+        {
+            // Arrange
+            var quantityAsDouble = double.Parse(quantity, NumberStyles.Any, CultureInfo.InvariantCulture);
+            recipeIngredient.Quantity = quantityAsDouble;
+
+            // Act
+            var result = _recipeIngredientValidator.TestValidate(recipeIngredient);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(ri => ri.Quantity);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidQuantity))]
+        public void RecipeIngredientQuantity_ForValidValues_ShouldNotHaveValidationError(string quantity)
+        {
+            // Arrange
+            var quantityAsDouble = double.Parse(quantity.Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture);
+            recipeIngredient.Quantity = quantityAsDouble;
+
+            // Act
+            var result = _recipeIngredientValidator.TestValidate(recipeIngredient);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(ri => ri.Quantity);
+        }
+
+        #endregion
     }
 }
