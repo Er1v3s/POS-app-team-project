45d208dca383173dbbe0f49fa027655b52e85cdf
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-01 22:46:26 +0100

    test: Prepare RecipeIngredientValidatorUnitTests class

---
+++ POS.Tests/UnitTests/RecipeIngredientValidatorUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Models;
+using POS.Validators.Models;
+
+namespace POS.Tests.UnitTests
+{
+    public class RecipeIngredientValidatorUnitTests
+    {
+        private readonly RecipeIngredientValidator _recipeIngredientValidator;
+
+        public RecipeIngredientValidatorUnitTests()
+        {
+            _recipeIngredientValidator = new RecipeIngredientValidator();
+        }
+
+        private RecipeIngredient recipeIngredient = new ()
+        {
+            Ingredient = new Ingredient() { Name = "Test name", Description = "Test description", Unit = "Test unit", Package = "Test package"},
+            Recipe = new Recipe { RecipeName = "Test name", RecipeContent = "Test content" },
+            Quantity = 0
+        };
+
+
+    }
+}
