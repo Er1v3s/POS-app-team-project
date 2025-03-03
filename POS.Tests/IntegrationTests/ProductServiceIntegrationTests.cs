e3bff8744a1fd80e7c2acb198e8559a32c985862
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-03 17:06:34 +0100

    test: Create an integration test class implementing base class

---
+++ POS.Tests/IntegrationTests/ProductServiceIntegrationTests.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using POS.Services.SalesPanel;
+
+namespace POS.Tests.IntegrationTests
+{
+    public class ProductServiceIntegrationTests : IntegrationTestBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductServiceIntegrationTests()
+        {
+            _productService = new ProductService(_dbContext, _databaseErrorHandlerMock.Object);
+        }
+
+        
+
+        protected override async Task SeedDatabase(AppDbContext dbContext)
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    ProductName = "Tom Collins",
+                    Category = "Whisky",
+                    Price = 25.99,
+                    Description = "Test description",
+                    Recipe = new Recipe { RecipeName = "Recipe name", RecipeContent = "Recipe content" }
+                },
+                new Product
+                {
+                    ProductName = "Negroni",
+                    Category = "Gin",
+                    Price = 23.99,
+                    Description = "Test description",
+                    Recipe = new Recipe { RecipeName = "Recipe name", RecipeContent = "Recipe content" }
+                },
+                new Product
+                {
+                    ProductName = "Mad dog",
+                    Category = "Wódka",
+                    Price = 18.99,
+                    Description = "Test description",
+                    Recipe = new Recipe { RecipeName = "Recipe name", RecipeContent = "Recipe content" }
+                }
+            };
+            await dbContext.Product.AddRangeAsync(products);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
