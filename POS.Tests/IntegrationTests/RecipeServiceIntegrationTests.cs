1d0bf4d68019deb53e4dac76236eb7b91bc8072a
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-04 16:38:09 +0100

    test: Create an integration test class implementing base class

---
+++ POS.Tests/IntegrationTests/RecipeServiceIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using POS.Services;
+
+namespace POS.Tests.IntegrationTests
+{
+    public class RecipeServiceIntegrationTests : IntegrationTestBase
+    {
+        private readonly RecipeService _recipeService;
+
+        public RecipeServiceIntegrationTests()
+        {
+            _recipeService = new RecipeService(_dbContext, _databaseErrorHandlerMock.Object);
+        }
+
+
+
+        protected override async Task SeedDatabase(AppDbContext dbContext)
+        {
+            List<Recipe> listOfRecipes = new()
+            {
+                new Recipe() { RecipeName = "Test recipe name", RecipeContent = "Test recipe content" },
+                new Recipe() { RecipeName = "Test recipe name 2", RecipeContent = "Test recipe content 2" },
+                new Recipe() { RecipeName = "Test recipe name 3", RecipeContent = "Test recipe content 3" },
+            };
+
+            await _dbContext.AddRangeAsync(listOfRecipes);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
