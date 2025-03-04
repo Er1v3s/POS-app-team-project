235ab83d35598ce55f7af614bf00862584fbfdd4
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-04 17:43:10 +0100

    test: Create an integration test to check functionality of getting recipe by id

--- POS.Tests/IntegrationTests/RecipeServiceIntegrationTests.cs
+++ POS.Tests/IntegrationTests/RecipeServiceIntegrationTests.cs
@@ -1,5 +1,10 @@
 ﻿using DataAccess;
 using DataAccess.Models;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using POS.Exceptions;
+using POS.Exceptions.Interfaces;
 using POS.Services;
 
 namespace POS.Tests.IntegrationTests
@@ -13,19 +18,62 @@ namespace POS.Tests.IntegrationTests
             _recipeService = new RecipeService(_dbContext, _databaseErrorHandlerMock.Object);
         }
 
+        [Fact]
+        public async Task GetRecipeByIdAsync_ForThePassedArgument_ReturnsRecipe()
+        {
+            // Arrange
+            _databaseErrorHandlerMock
+                .Setup(x => x.ExecuteDatabaseOperationAsync(It.IsAny<Func<Task<Recipe>>>(), It.IsAny<Action>()))
+                .Returns<Func<Task<Recipe>>, Action<Exception>>((operation, onFailure) => operation());
 
+            var existingRecipe = await _dbContext.Recipe.FirstOrDefaultAsync();
+            if (existingRecipe is null) throw new NotFoundException();
+
+            // Act
+            var recipeService = new RecipeService(_dbContext, _databaseErrorHandlerMock.Object);
+            var recipe = await recipeService.GetRecipeByIdAsync(existingRecipe.RecipeId);
+
+            // Assert
+            recipe.Should().NotBeNull();
+            recipe.RecipeId.Should().Be(existingRecipe.RecipeId);
+            recipe.RecipeIngredients.Should().NotBeNullOrEmpty();
+            recipe.RecipeIngredients.First().Ingredient.Should().NotBeNull();
+        }
 
         protected override async Task SeedDatabase(AppDbContext dbContext)
         {
-            List<Recipe> listOfRecipes = new()
+            var ingredient = new Ingredient()
             {
-                new Recipe() { RecipeName = "Test recipe name", RecipeContent = "Test recipe content" },
-                new Recipe() { RecipeName = "Test recipe name 2", RecipeContent = "Test recipe content 2" },
-                new Recipe() { RecipeName = "Test recipe name 3", RecipeContent = "Test recipe content 3" },
+                Name = "Whisky",
+                Description = "Jack Daniel's",
+                Unit = "szt",
+                Package = "Szklana butelka 700ml",
+                Stock = 10,
+                SafetyStock = 5
             };
 
-            await _dbContext.AddRangeAsync(listOfRecipes);
-            await _dbContext.SaveChangesAsync();
+            var recipe = new Recipe()
+            {
+                RecipeName = "Test recipe name",
+                RecipeContent = "Test recipe content",
+            };
+
+            var recipeIngredients = new List<RecipeIngredient>()
+            {
+                new RecipeIngredient
+                {
+                    Ingredient = ingredient,
+                    Quantity = 50,
+                    Recipe = recipe
+                },
+            };
+
+            recipe.RecipeIngredients = recipeIngredients;
+
+            await dbContext.Ingredients.AddAsync(ingredient);
+            await dbContext.Recipe.AddAsync(recipe);
+            await dbContext.RecipeIngredients.AddRangeAsync(recipeIngredients);
+            await dbContext.SaveChangesAsync();
         }
     }
 }
