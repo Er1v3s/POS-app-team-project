ed60ebc4814f81cc5f60d9b6b8eca1cd78c9cd93
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-07 21:21:37 +0100

    test: Create a unit test of Initializing DeliveryCollection

---
+++ POS.Tests/UnitTests/WarehouseFunctions/DeliveryServiceUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Models;
+using FluentAssertions;
+using POS.Models.Warehouse;
+using POS.Services.WarehouseFunctions;
+
+namespace POS.Tests.UnitTests.WarehouseFunctions
+{
+    public class DeliveryServiceUnitTests
+    {
+        [Fact]
+        public void DeliveryService_OnServiceInitialize_DeliveryCollectionShouldNotBeNull()
+        {
+            // Act
+            var deliveryService = new DeliveryService();
+
+            // Assert
+            deliveryService.DeliveryCollection.Should().NotBeNull();
+        }
+
+        
+    }
+}
