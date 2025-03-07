f196af0a0bf2ed42a59fdf947fbd9b33e4d896de
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-07 21:23:39 +0100

    test: Create a unit test that checks 'increasing ingredient quantity in delivery' method

--- POS.Tests/UnitTests/WarehouseFunctions/DeliveryServiceUnitTests.cs
+++ POS.Tests/UnitTests/WarehouseFunctions/DeliveryServiceUnitTests.cs
@@ -17,6 +17,26 @@ namespace POS.Tests.UnitTests.WarehouseFunctions
             deliveryService.DeliveryCollection.Should().NotBeNull();
         }
 
+        [Fact]
+        public void IncreaseIngredientQuantityInDelivery_ForExistingIngredient_ShouldIncreaseQuantityByOne()
+        {
+            // Arrange
+            var deliveryService = new DeliveryService();
+            var deliveryItem = new DeliveryDto
+            {
+                Quantity = 1,
+                Ingredient = new Ingredient() { Name = "Whisky", Description = "Jack Daniel's", Unit = "szt", Package = "Szklana butelka 700ml", Stock = 10, SafetyStock = 5 },
+            };
+            deliveryService.DeliveryCollection.Add(deliveryItem);
+
+            // Act
+            deliveryService.IncreaseIngredientQuantityInDelivery(deliveryItem);
+
+            // Assert
+            deliveryService.DeliveryCollection.Should().HaveCount(1);
+            deliveryService.DeliveryCollection[0].Quantity.Should().Be(2);
+        }
+
         
     }
 }
